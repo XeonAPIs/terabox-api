9c4c8d7f9669df8c07c25a9e82186ee3fe4eb5b0
Hye Vro! <xeonapi@gmail.com>
2026-05-28 16:39:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-```python
 from flask import Flask, request, jsonify
 import requests
 import re
@@ -162,4 +161,3 @@ def tera():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-```
